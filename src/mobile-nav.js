04fd02cd60c652cbc7b59455a42a7fad735e3026
Vishesh Verma <visheshverma250703@gmail.com>
2024-06-04 00:05:35 +0530

    bug fixes

--- src/mobile-nav.js
+++ src/mobile-nav.js
@@ -5,17 +5,21 @@ const mobileNav = () => {
   
     // State
     let isMobileNavOpen = false;
+    const light = "#f1f5f9"
+    const dark = "#070a13"
   
     headerBtn.addEventListener('click', () => {
       isMobileNavOpen = !isMobileNavOpen;
       if (isMobileNavOpen) {
         mobileNav.style.display = 'flex';
         document.body.style.overflowY = 'hidden';
-        headerBtn.style.color = "white"
-      } else {
+        headerBtn.style.color = light;
+    } else {
         mobileNav.style.display = 'none';
         document.body.style.overflowY = 'auto';
+        headerBtn.style.color = dark;
       }
+    
     });
   
     mobileLinks.forEach(link => {
