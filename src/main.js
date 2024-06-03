535a80837940e8cf0b8cdeb12b080d4ce752723c
Vishesh Verma <visheshverma250703@gmail.com>
2024-06-03 23:36:28 +0530

    added complete project

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import '../styles/mordern-normalize.css'
+import '../styles/style.css'
+import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/features.css'
+import '../styles/components/testimonals.css'
+import '../styles/components/endframe.css'
+import '../styles/components/footer.css'
+import '../styles/components/faq.css'
+import '../styles/components/mobile-nav.css'
+import '../styles/utils.css'
+import mobileNav from './mobile-nav'
+
+mobileNav();
